6371edcb6cc11e5006204bd470b8c030f0dc4836
Zhabbarov.Ramis <Zhabbarov.Ramis@wildberries.ru>
2023-05-17 20:36:09 +0300

    service workers

--- public/sw.js
+++ public/sw.js
@@ -1,36 +1,36 @@
-const CACHE = 'service-worker-cache';
-const timeout = 400;
-
-self.addEventListener('install', (event) => {
-  console.log('Установлен');
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('Активирован');
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fromNetwork(event.request, timeout)
-      .catch((err) => {
-        console.log(`Error: ${err.message()}`);
-        return fromCache(event.request);
-      }));
-});
-
-// Запрос ограничееный по времени
-const fromNetwork = (request, timeout) => {
-  return new Promise((resolve, reject) => {
-    const timeoutId = setTimeout(reject, timeout);
-    fetch(request).then((response) => {
-      clearTimeout(timeoutId);
-      resolve(response);
-    }, reject);
-  });
-};
-
-const fromCache = (request) => {
-  return caches.open(CACHE).then((cache) =>
-      cache.match(request).then((matching) =>
-          matching || Promise.reject('no-match')
-      ));
-};
+// const CACHE = 'service-worker-cache';
+// const timeout = 400;
+//
+// self.addEventListener('install', (event) => {
+//   console.log('Установлен');
+// });
+//
+// self.addEventListener('activate', (event) => {
+//   console.log('Активирован');
+// });
+//
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(fromNetwork(event.request, timeout)
+//       .catch((err) => {
+//         console.log(`Error: ${err.message()}`);
+//         return fromCache(event.request);
+//       }));
+// });
+//
+// // Запрос ограничееный по времени
+// const fromNetwork = (request, timeout) => {
+//   return new Promise((resolve, reject) => {
+//     const timeoutId = setTimeout(reject, timeout);
+//     fetch(request).then((response) => {
+//       clearTimeout(timeoutId);
+//       resolve(response);
+//     }, reject);
+//   });
+// };
+//
+// const fromCache = (request) => {
+//   return caches.open(CACHE).then((cache) =>
+//       cache.match(request).then((matching) =>
+//           matching || Promise.reject('no-match')
+//       ));
+// };
